c312c418ec71f9de8cfd659e8087f836fd6010c4
zcmjs <zcm@interia.pl>
2020-01-02 18:51:15 +0100

    init commit

---
+++ src/main/java/zcmee/com/github/FullTesing/FullTesingApplication.java
@@ -0,0 +1,13 @@
+package zcmee.com.github.FullTesing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullTesingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullTesingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zcmee/com/github/FullTesing/FullTesingApplicationTests.java
@@ -0,0 +1,13 @@
+package zcmee.com.github.FullTesing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullTesingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
